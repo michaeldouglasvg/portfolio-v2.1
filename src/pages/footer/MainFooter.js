02ac9f1ca18901cdc926cd282567070f4341e83b
michaeldouglasvg <douglas1999michael@gmail.com>
2024-02-19 21:28:36 +0300

    Dark Mode

--- src/pages/footer/MainFooter.js
+++ src/pages/footer/MainFooter.js
@@ -3,15 +3,15 @@ import { FaFacebook, FaGithub, FaInstagram, FaTelegram, FaTwitter, FaWhatsapp } 
 
 const MainFooter = () => {
   return (
-    <div className='Footermain'>
-      <div className="images">
+    <div className='Footermain' style={{background: "#3586ff"}}>
+      <div className="images" style={{background: "#3586ff"}}>
         <div className="wave" id="wave-1"></div>
         <div className="wave" id="wave-2"></div>
         <div className="wave" id="wave-3"></div>
         <div className="wave" id="wave-4"></div>
       </div>
-      <div className="footer-content">
-        <div className="icons-section">
+      <div className="footer-content" style={{background: "#3586ff"}}>
+        <div className="icons-section" style={{background: "#3586ff"}}>
             <FaFacebook className='Footericons'/>
             <FaTwitter className='Footericons'/>
             <FaGithub className='Footericons'/>
@@ -19,14 +19,14 @@ const MainFooter = () => {
             <FaWhatsapp className='Footericons'/>
             <FaTelegram className='Footericons'/>
         </div>
-        <div className="links">
+        <div className="links" style={{background: "#3586ff"}}>
             <p>Home</p>
             <p>Services</p>
             <p>Projects</p>
             <p>Contacts</p>
             <p>Blocks</p>
         </div>
-        <div className="copyright">
+        <div className="copyright" style={{background: "#3586ff"}}>
             <p>Copyright&copy;2023Gtechmb.com || All rights reserved.</p>
         </div>
       </div>
